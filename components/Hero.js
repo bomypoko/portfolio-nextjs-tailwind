110e809e0aae4b1a8c622eb4199f936dc3b4536e
Ratchasri Kidking <orc_hero@hotmail.com>
2022-08-09 18:00:49 +0700

    Update Hero section

---
+++ components/Hero.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+    AiOutlineCloseCircle,
+    AiOutlineMail,
+    AiFillGithub,
+    AiFillFacebook,
+  } from "react-icons/Ai";
+import { FaLine } from "react-icons/Fa";
+
+const Hero = () => {
+  return (
+    <div className=" w-full h-screen text-center ">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest border-b-2 shadow-b py-5 text-gray-400 ">
+            A Front-End Developer with a passion for learning and Creating.{" "}
+          </p>
+          <h1>
+            {" "}
+            Hello, I'm{" "}
+            <span className="bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500 bg-clip-text text-transparent">
+              {" "}
+              Ratchasri Kidking{" "}
+            </span>{" "}
+          </h1>
+          <p className="py-5 text-gray-400 max-w-[70%] m-auto ">
+            {" "}
+            I'am a self-tought and start my journey for coding at the begining
+            of this year. Now I'm focuzing on building responsive front-end web
+            application while also learning back-end technologies.{" "}
+          </p>
+
+          <div className=" flex items-center justify-between max-w-[320px] mx-auto py-10 ">
+            <div className="rounded-md shadow-xl shadow-gray-500 cursor-pointer hover:scale-125 duration-200">
+              <AiOutlineMail size={40} />
+            </div>
+            <div className="rounded-md shadow-xl shadow-gray-500 cursor-pointer hover:scale-125 duration-200">
+              <FaLine size={40} />
+            </div>
+            <div className="rounded-md shadow-xl shadow-gray-500 cursor-pointer hover:scale-125 duration-200">
+              <AiFillFacebook size={40} />
+            </div>
+            <div className="rounded-full shadow-xl shadow-gray-500 cursor-pointer hover:scale-125 duration-200">
+              <AiFillGithub size={40} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/Navbar.js
@@ -0,0 +1,147 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/Gi";
+import {
+  AiOutlineCloseCircle,
+  AiOutlineMail,
+  AiFillGithub,
+  AiFillFacebook,
+} from "react-icons/Ai";
+import { FaLine } from "react-icons/Fa";
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <div className=" fixed w-full h-30 shadow-xl z-[100] ">
+      <div className="flex w-full h-full justify-between items-center px-2 2xl:16">
+        <Image
+          src="/../public/assets/bomLogo.png"
+          alt=""
+          width={150}
+          height={150}
+        />
+
+        <div>
+          <ul className="hidden md:flex space-x-5  ">
+            <Link href="/">
+              <li className="text-sm xl:text-2xl capitalize rouned-full hover:translate-y-[-20%] duration-500 hover:border-b hover:border-red-300 cursor-pointer p-2">
+                {" "}
+                Home{" "}
+              </li>
+            </Link>
+            <Link href="/">
+              <li className=" text-sm xl:text-2xl  capitalize hover:translate-y-[-20%] duration-500 hover:border-b hover:border-red-300 cursor-pointer p-2">
+                {" "}
+                About Me{" "}
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="text-sm xl:text-2xl capitalize hover:translate-y-[-20%] duration-500 hover:border-b hover:border-red-300 cursor-pointer p-2">
+                {" "}
+                Expericense{" "}
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="text-sm  xl:text-2xl  capitalize hover:translate-y-[-20%] duration-500 hover:border-b hover:border-red-300 cursor-pointer p-2">
+                {" "}
+                Portfolio{" "}
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="text-sm  xl:text-2xl capitalize hover:translate-y-[-20%] duration-500 hover:border-b hover:border-red-300 cursor-pointer p-2">
+                {" "}
+                Contact{" "}
+              </li>
+            </Link>
+          </ul>
+          <div onClick={handleToggle} className="md:hidden  ">
+            <GiHamburgerMenu size={30} />
+          </div>
+        </div>
+      </div>
+      <div
+        className={
+          toggle
+            ? "bg-white/50 fixed h-screen w-full left-0 top-0 md:hidden"
+            : ""
+        }
+      >
+        <div
+          className={
+            toggle
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[20%] bg-black h-screen p-10 ease-in duration-500"
+              : "fixed left-[-100%] top-0 w-[75%] sm:w-[60%] md:w-[20%] bg-black h-screen p-10 ease-in duration-500"
+          }
+        >
+          <div className="flex w-full items-center justify-between">
+            <Image
+              src="/../public/assets/bomLogo.png "
+              alt=""
+              width={100}
+              height={100}
+            />
+
+            <div
+              onClick={handleToggle}
+              className="shadow-gray-500 shadow-lg rounded-full "
+            >
+              <AiOutlineCloseCircle size={40} />
+            </div>
+          </div>
+
+          <div className="text-transparent text-md bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 mt-3 border-b-gray-500 border-b capitalize">
+            <p className=" w-[80%] md:w-[100%] py-3">
+              Let me create your website
+            </p>
+          </div>
+
+          <div className="py-2 flex flex-col  ">
+            <ul>
+              <Link href="/">
+                <li className="tracking-wide  py-3"> Home </li>
+              </Link>
+              <Link href="/">
+                <li className="tracking-wider py-3"> About Me </li>
+              </Link>
+              <Link href="/">
+                <li className="tracking-wider py-3"> Expericense</li>
+              </Link>
+              <Link href="/">
+                <li className="tracking-wider py-3"> Portfolio </li>
+              </Link>
+              <Link href="/">
+                <li className="tracking-wider pt-3"> Contact </li>
+              </Link>
+            </ul>
+
+            <div className=" text-transparent text-md bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  border-b-gray-500 border-b py-2 ">
+              <p>Contact me at </p>
+            </div>
+
+            <div className="flex items-center justify-between mt-1 w-full sm:w-[80%]">
+              <div className="rounded-md shadow-xl shadow-gray-500 cursor-pointer hover:scale-125 duration-200">
+                <AiOutlineMail size={40} />
+              </div>
+              <div className="rounded-md shadow-xl shadow-gray-500 cursor-pointer hover:scale-125 duration-200">
+                <FaLine size={40} />
+              </div>
+              <div className="rounded-md shadow-xl shadow-gray-500 cursor-pointer hover:scale-125 duration-200">
+                <AiFillFacebook size={40} />
+              </div>
+              <div className="rounded-full shadow-xl shadow-gray-500 cursor-pointer hover:scale-125 duration-200">
+                <AiFillGithub size={40} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
